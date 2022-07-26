d83f8f7f584b11420b675329c838a72a9d0c5b69
Zhihang Liang <command1748165360@126.com>
2022-07-26 17:02:31 +0800

    qyl: 重新定义uuid列表，重构内部函数

--- stack.c
+++ stack.c
@@ -2,22 +2,144 @@
 /**
  * 文件名：stack.c
  * 作者：秋叶零
- * 日期：2022年7月24日
+ * 创建日期：2022年7月24日
+ * 修改日期：2022年7月26日
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "stack.h"
 
 #define T stack_T
+typedef struct T *T;
 
-void _stack_init_uuid_list() {
-    _uuid_list = (struct _UuidList *)malloc(sizeof(struct _UuidList));
-    _uuid_list->count = 0;
-    _uuid_list->size = _UUID_COUNT;
-    _uuid_list->uuid = (char **)malloc(sizeof(char *) * _UUID_COUNT);
+T stack_new(int size) {
+    assert(size > 0);
+
+    T stk = (T)malloc(sizeof(struct stack_T));
+    stk->count = 0;
+    stk->size = size;
+    stk->head = NULL;
+
+    FILE *fp = fopen("/proc/sys/kernel/random/uuid", "r");
+    fgets(stk->uuid, _MAX_UUID_LENGTH, fp);
+    _stack_add_uuid(stk->uuid);
+
+    fclose(fp);
+
+    return stk;
+}
+
+void stack_push(T stk, void *x) {
+    assert(stk != NULL);
+    assert(x != NULL);
+    assert(_stack_check_uuid(stk));
+    assert(stk->count < stk->size);
+
+    stk->count++;
+    
+    struct elem *new_elem = (struct elem *)malloc(sizeof(struct elem));
+    new_elem->x = x;
+    new_elem->next = stk->head;
+    stk->head = new_elem;
+
     return;
+}
+
+void *stack_pop(T stk) {
+    assert(stk != NULL);
+    assert(_stack_check_uuid(stk));
+    assert(stk->count > 0);
+
+    stk->count--;
+
+    struct elem *del = stk->head;
+    void *x = del->x;
+    stk->head = del->next;
+    free(del);
+    del = NULL;
+
+    return x;
+}
+
+int stack_is_empty(T stk) {
+    assert(stk != NULL);
+    assert(_stack_check_uuid(stk));
+
+    return stk->count == 0;
+}
+
+void stack_free(T *stk) {
+    assert(stk != NULL);
+    assert(_stack_check_uuid(stk));
+
+    _stack_delete_uuid((*stk)->uuid);
+
+    struct elem *prev = (*stk)->head, *next;
+    while(prev->next != NULL) {
+        next = prev->next;
+        free(prev);
+        prev = next;
+    }
+    free(prev);
+    free(*stk);
+
+    return;
+}
+
+void _stack_add_uuid(const char *uuid) {
+    assert(uuid != NULL);
+
+    struct _UUIDList *new_uuid = (struct _UUIDList *)malloc(sizeof(struct _UUIDList));
+    strncpy(new_uuid->uuid, uuid, _MAX_UUID_LENGTH);
+
+    new_uuid->next = _uuid_list_head;
+    _uuid_list_head = new_uuid;
+
+    return;
+}
+
+void _stack_delete_uuid(const char *uuid) {
+    assert(uuid != NULL);
+
+    struct _UUIDList *prev, *del = _uuid_list_head;
+
+    if(del == NULL) {
+        return;
+    }
+
+    if(del->next == NULL) {
+        free(del);
+        _uuid_list_head = NULL;
+    }
+
+    while(del != NULL) {
+        prev = del;
+        del = del->next;
+        if(del != NULL && strcmp(del->uuid, uuid)) {
+            prev->next = del->next;
+            free(del);
+        }
+    }
+
+    return;
+}
+
+int _stack_check_uuid(const char *uuid) {
+    assert(uuid != NULL);
+
+    struct _UUIDList *p = _uuid_list_head;
+
+    while(p != NULL) {
+        if(strcmp(p->uuid, uuid)) {
+            return 1;
+        }
+        p = p->next;
+    }
+
+    return 0;
 }
 
 #undef T
--- stack.h
+++ stack.h
@@ -2,47 +2,48 @@
 /**
  * 文件名：stack.h
  * 作者：秋叶零
- * 日期：2022年7月24日
+ * 创建日期：2022年7月24日
+ * 修改日期：2022年7月26日
  */
 
 #ifndef _STACK_H_
 #define _STACK_H_
 
 #define _MAX_UUID_LENGTH  256
-#define _UUID_COUNT       32
+
+// 栈中的元素
+struct elem {
+    void *x;
+    struct elem *next;
+};
 
 // stack的数据结构
 #define T stack_T
 struct T {
-    int count;
-    int size;
-    char uuid[_MAX_UUID_LENGTH];
-    struct elem {
-        void *x;
-        struct elem *list;
-    } *head;
+    int count;				// 栈中元素个数
+    int size;				// 栈的大小
+    char uuid[_MAX_UUID_LENGTH];	// 栈的唯一标识符
+    struct elem *head;			// 栈底元素
 };
 typedef struct T *T;
 
 // uuid列表（内部使用，不提供对外接口）
-struct _UuidList {
-    int count;
-    int size;
-    char **uuid;
-} *_uuid_list = 0;
+struct _UUIDList {
+    char uuid[_MAX_UUID_LENGTH];
+    struct _UUIDList *next;
+} *_uuid_list_head = (struct _UUIDList *)0;
 
 // 对外提供的接口
-extern T    stack_new                   (int size);
-extern void stack_push                  (T stk, void *x);
-extern T    stack_pop                   (T stk);
-extern int  stack_is_empty              (T stk);
-extern void stack_free                  (T stk);
+extern T     stack_new       (int size);
+extern void  stack_push      (T stk, void *x);
+extern void *stack_pop       (T stk);
+extern int   stack_is_empty  (T stk);
+extern void  stack_free      (T *stk);
 
 // 内部函数
-extern void _stack_init_uuid_list       ();
-extern int  _stack_check_uuid           (T stk, struct _UuidList *uuid_list);
-extern void _stack_realloc_uuid_list    (struct _UuidList *uuid_list);
-extern void _stack_free_uuid_list       (struct _UuidList *uuid_list);
+extern void _stack_add_uuid     (const char *uuid);
+extern void _stack_delete_uuid  (const char *uuid);
+extern int _stack_check_uuid    (const char *uuid);
 
 #undef T
-#endif
+#endif // _STACK_H_
