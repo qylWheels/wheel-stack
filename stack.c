125382e3f6eba1cf432426a18365335dfebaf173
Zhihang Liang <command1748165360@126.com>
2022-07-28 11:39:07 +0800

    qyl: 隐藏了内部函数与结构体的内部细节

--- stack.c
+++ stack.c
@@ -2,8 +2,7 @@
 /**
  * 文件名：stack.c
  * 作者：秋叶零
- * 创建日期：2022年7月24日
- * 修改日期：2022年7月26日
+ * 日期：2022年7月28日
  */
 
 #include <stdio.h>
@@ -12,8 +11,32 @@
 #include <assert.h>
 #include "stack.h"
 
+// 内部函数声明
+void _stack_add_uuid(const char *uuid);
+void _stack_delete_uuid(const char *uuid);
+int _stack_check_uuid(const char *uuid);
+
+// 栈中的元素（内部使用，不提供对外接口）
+struct elem {
+    void *x;
+    struct elem *next;
+};
+
+// stack的数据结构
 #define T stack_T
+struct T {
+    int count;				// 栈中元素个数
+    int size;				// 栈的大小
+    char uuid[_MAX_UUID_LENGTH];	// 栈的唯一标识符
+    struct elem *head;			// 栈底元素
+};
 typedef struct T *T;
+
+// uuid列表（内部使用，不提供对外接口）
+struct _UUIDList {
+    char uuid[_MAX_UUID_LENGTH];
+    struct _UUIDList *next;
+} *_uuid_list_head = NULL;
 
 T stack_new(int size) {
     assert(size > 0);
@@ -35,7 +58,7 @@ T stack_new(int size) {
 void stack_push(T stk, void *x) {
     assert(stk != NULL);
     assert(x != NULL);
-    assert(_stack_check_uuid(stk));
+    assert(_stack_check_uuid(stk->uuid));
     assert(stk->count < stk->size);
 
     stk->count++;
@@ -50,7 +73,7 @@ void stack_push(T stk, void *x) {
 
 void *stack_pop(T stk) {
     assert(stk != NULL);
-    assert(_stack_check_uuid(stk));
+    assert(_stack_check_uuid(stk->uuid));
     assert(stk->count > 0);
 
     stk->count--;
@@ -66,14 +89,14 @@ void *stack_pop(T stk) {
 
 int stack_is_empty(T stk) {
     assert(stk != NULL);
-    assert(_stack_check_uuid(stk));
+    assert(_stack_check_uuid(stk->uuid));
 
     return stk->count == 0;
 }
 
 void stack_free(T *stk) {
     assert(stk != NULL);
-    assert(_stack_check_uuid(stk));
+    assert(_stack_check_uuid((*stk)->uuid));
 
     _stack_delete_uuid((*stk)->uuid);
 
@@ -89,6 +112,7 @@ void stack_free(T *stk) {
     return;
 }
 
+// 以下以“_”开头的函数均为内部函数，不提供对外接口，以隐藏细节
 void _stack_add_uuid(const char *uuid) {
     assert(uuid != NULL);
 
--- stack.h
+++ stack.h
@@ -2,8 +2,7 @@
 /**
  * 文件名：stack.h
  * 作者：秋叶零
- * 创建日期：2022年7月24日
- * 修改日期：2022年7月26日
+ * 日期：2022年7月28日
  */
 
 #ifndef _STACK_H_
@@ -11,27 +10,10 @@
 
 #define _MAX_UUID_LENGTH  256
 
-// 栈中的元素
-struct elem {
-    void *x;
-    struct elem *next;
-};
-
-// stack的数据结构
+// stack的数据结构（仅声明，不实现，隐藏细节）
 #define T stack_T
-struct T {
-    int count;				// 栈中元素个数
-    int size;				// 栈的大小
-    char uuid[_MAX_UUID_LENGTH];	// 栈的唯一标识符
-    struct elem *head;			// 栈底元素
-};
+struct T;
 typedef struct T *T;
-
-// uuid列表（内部使用，不提供对外接口）
-struct _UUIDList {
-    char uuid[_MAX_UUID_LENGTH];
-    struct _UUIDList *next;
-} *_uuid_list_head = (struct _UUIDList *)0;
 
 // 对外提供的接口
 extern T     stack_new       (int size);
@@ -39,11 +21,6 @@ extern void  stack_push      (T stk, void *x);
 extern void *stack_pop       (T stk);
 extern int   stack_is_empty  (T stk);
 extern void  stack_free      (T *stk);
-
-// 内部函数
-extern void _stack_add_uuid     (const char *uuid);
-extern void _stack_delete_uuid  (const char *uuid);
-extern int _stack_check_uuid    (const char *uuid);
 
 #undef T
 #endif // _STACK_H_
